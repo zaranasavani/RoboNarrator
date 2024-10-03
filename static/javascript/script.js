1258f39321f72ca7d994682535948873b26d73d1
zaranasavani <zarana.2002.savani@gmail.com>
2024-10-03 12:56:59 +0530

    comment added

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -4,13 +4,16 @@ const playButton = document.getElementById('playBtn');
 const pauseButton = document.getElementById('pauseBtn');
 const stopButton = document.getElementById('stopBtn');
 const resumeButton = document.getElementById('resumeBtn');
-let speechSynthesis = window.speechSynthesis;
-let utterance; // Store the utterance for playback control
-let currentIndex = 0; // Index for highlighted words
-let isSpeaking = false; // Flag to track if speaking
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
+// speechSynthesis API create instance of browser's speech synthesis API, used to convert text to speech
+let speechSynthesis = window.speechSynthesis;
+
+let utterance; // used to store instance of SpeechSynthesisUtterance, represents the speech request. It holds the text and settings related to the speech.
+let currentIndex = 0;                                                    // Index for highlighted words
+let isSpeaking = false;                                                  // Flag to track if speaking
+
+form.addEventListener('submit', function (e) {                           // e : event object for the form submission
+    e.preventDefault();                                                  // ensures the form is handled via JavaScript without reloading the page
     const text = document.getElementById('text').value;
     highlightText(text);
     speakText(text);
@@ -18,24 +21,24 @@ form.addEventListener('submit', function (e) {
 
 function highlightText(text) {
     const words = text.split(' ').map(word => `<span>${word} </span>`).join('');
-    highlightedTextDiv.innerHTML = words; // Show the highlighted words
-    highlightedTextDiv.style.display = 'block'; // Display the highlighted text div
+    highlightedTextDiv.innerHTML = words;                                     // Show the highlighted words
+    highlightedTextDiv.style.display = 'block';                               // Display the highlighted text div
 }
 
 function speakText(text) {
     // Reset previous utterance
     if (utterance) {
-        speechSynthesis.cancel(); // Cancel previous speech if any
+        speechSynthesis.cancel();                                             // Cancel previous speech if any
     }
 
     utterance = new SpeechSynthesisUtterance(text);
 
     utterance.onstart = function () {
-        isSpeaking = true; // Update speaking flag
-        playButton.style.display = 'none'; // Hide play button
-        pauseButton.style.display = 'inline-block'; // Show pause button
-        stopButton.style.display = 'inline-block'; // Show stop button
-        resumeButton.style.display = 'none'; // Hide resume button
+        isSpeaking = true;                                                  // Update speaking flag
+        playButton.style.display = 'none';                                  // Hide play button
+        pauseButton.style.display = 'inline-block';                         // Show pause button
+        stopButton.style.display = 'inline-block';                          // Show stop button
+        resumeButton.style.display = 'none';                                // Hide resume button
     };
 
     // Event handler for when a word is being spoken
@@ -53,20 +56,20 @@ function speakText(text) {
     // Event handler for when speech has finished
     utterance.onend = function () {
         const spans = highlightedTextDiv.querySelectorAll('span');
-        spans.forEach(span => span.classList.remove('highlight')); // Remove highlights
-        currentIndex = 0; // Reset current index
-        isSpeaking = false; // Update speaking flag
+        spans.forEach(span => span.classList.remove('highlight'));              // Remove highlights
+        currentIndex = 0;                                                       // Reset current index
+        isSpeaking = false;                                                     // Update speaking flag
 
         // Display message after speech has finished
         const messageDiv = document.createElement('div');
         messageDiv.classList.add('alert', 'alert-info', 'mt-3');
         messageDiv.textContent = 'Speech has been spoken';
-        highlightedTextDiv.appendChild(messageDiv); // Append message to highlighted text div
+        highlightedTextDiv.appendChild(messageDiv);
 
-        playButton.style.display = 'inline-block'; // Show play button
-        pauseButton.style.display = 'none'; // Hide pause button
-        stopButton.style.display = 'none'; // Hide stop button
-        resumeButton.style.display = 'none'; // Hide resume button
+        playButton.style.display = 'inline-block'; 
+        pauseButton.style.display = 'none'; 
+        stopButton.style.display = 'none'; 
+        resumeButton.style.display = 'none'; 
     };
 
     // Speak the text
@@ -76,43 +79,43 @@ function speakText(text) {
 // Play audio functionality
 playButton.addEventListener('click', function () {
     if (isSpeaking) {
-        speechSynthesis.resume(); // Resume if paused
+        speechSynthesis.resume();                                      // Resume if paused
     } else {
-        speechSynthesis.speak(utterance); // Speak the text again
+        speechSynthesis.speak(utterance);                              // Speak the text again
     }
-    playButton.style.display = 'none'; // Hide play button
-    pauseButton.style.display = 'inline-block'; // Show pause button
-    stopButton.style.display = 'inline-block'; // Show stop button
-    resumeButton.style.display = 'none'; // Hide resume button
+    playButton.style.display = 'none';                                 
+    pauseButton.style.display = 'inline-block';                        
+    stopButton.style.display = 'inline-block';                         
+    resumeButton.style.display = 'none';                               
 });
 
 // Pause audio functionality
 pauseButton.addEventListener('click', function () {
-    speechSynthesis.pause(); // Pause the speech
-    playButton.style.display = 'inline-block'; // Show play button
-    pauseButton.style.display = 'none'; // Hide pause button
-    stopButton.style.display = 'inline-block'; // Show stop button
-    resumeButton.style.display = 'inline-block'; // Show resume button
+    speechSynthesis.pause();                                            // Pause the speech
+    playButton.style.display = 'inline-block'; 
+    pauseButton.style.display = 'none'; 
+    stopButton.style.display = 'inline-block'; 
+    resumeButton.style.display = 'inline-block'; 
 });
 
 // Stop audio functionality
 stopButton.addEventListener('click', function () {
-    speechSynthesis.cancel(); // Stop the speech
+    speechSynthesis.cancel();                                            // Stop the speech
     const spans = highlightedTextDiv.querySelectorAll('span');
-    spans.forEach(span => span.classList.remove('highlight')); // Remove highlights
-    currentIndex = 0; // Reset current index
-    playButton.style.display = 'inline-block'; // Show play button
-    pauseButton.style.display = 'none'; // Hide pause button
-    stopButton.style.display = 'none'; // Hide stop button
-    resumeButton.style.display = 'none'; // Hide resume button
-    isSpeaking = false; // Update speaking flag
+    spans.forEach(span => span.classList.remove('highlight'));           // Remove highlights
+    currentIndex = 0;                                                    // Reset current index
+    playButton.style.display = 'inline-block'; 
+    pauseButton.style.display = 'none'; 
+    stopButton.style.display = 'none'; 
+    resumeButton.style.display = 'none'; 
+    isSpeaking = false;                                                  // Update speaking flag
 });
 
 // Resume audio functionality
 resumeButton.addEventListener('click', function () {
-    speechSynthesis.resume(); // Resume the speech
-    playButton.style.display = 'none'; // Hide play button
-    pauseButton.style.display = 'inline-block'; // Show pause button
-    stopButton.style.display = 'inline-block'; // Show stop button
-    resumeButton.style.display = 'none'; // Hide resume button
+    speechSynthesis.resume();                                            // Resume the speech
+    playButton.style.display = 'none'; 
+    pauseButton.style.display = 'inline-block';
+    stopButton.style.display = 'inline-block'; 
+    resumeButton.style.display = 'none';
 });
