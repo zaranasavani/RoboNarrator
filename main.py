180461f5d20fc778e8350ab926b18c24a48baa4e
zaranasavani <zarana.2002.savani@gmail.com>
2025-05-14 20:13:47 +0200

    change in main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, render_template     # flask ==> create web application  || request ==> to get data from form || render_template ==> to render html file
 import pyttsx3
+import os
+
+port = int(os.environ.get("PORT", 5000))
 
 app = Flask(__name__)                                 # create flask application object
 
@@ -16,4 +19,4 @@ def speak():
     return render_template('index.html', message="Text has been spoken!")
 
 if __name__ == '__main__':                            # insure app runs when script is executed
-    app.run(debug=True) 
+    app.run(host="0.0.0.0", port=port, debug=True) 
